b0634fe936f21918a553aafca4b8d97678fb4369
pamefer95 <pamefer.95@gmail.com>
2018-07-28 22:17:13 -0500

    Apply services to functions

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css'
-import convert from 'convert-units';
+import transformWeather from '../../services/transformWeather';
 import { 
     SUN
 } from '../../constants/weathers';
+
 
 const location = "Loja, ec"
 const api_key = "14c300da705692c2d414d00e13d90170"
@@ -27,33 +28,12 @@ class WeatherLocation extends Component {
         };
     }
 
-    getTemp = kelvin => {
-        return convert(kelvin).from('K').to('C').toFixed(2);
-    }
-    getWeatherState = weather => {
-        return SUN;
-    }
-
-    getData = (weather_data) => {
-        const { humidity, temp } = weather_data.main;
-        const { speed } = weather_data.wind;
-        const weatherState  = this.getWeatherState(this.weather);
-        const temperature =  this.getTemp(temp);
-
-        const data = {
-            humidity,               // the same as humidity : humidity ECMA6
-            temperature,
-            weatherState,
-            wind: `${speed} m/s`
-        }
-        return data;
-    } 
     handleUpdateClick = () => {
         fetch(api_weather).then( data => {
             return data.json();
         }).then( weather_data => {
             debugger;
-            const data = this.getData(weather_data);
+            const data = transformWeather(weather_data);
             this.setState({ data })     // the same as data : data ECMA6
         });
     }
